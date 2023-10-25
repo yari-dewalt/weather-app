540ff6291992dd02aea0dbdbd3658849d99b9205
yari-dewalt <yari.dewalt@gmail.com>
2023-10-24 20:36:44 -0700

    Setup simple format to test weather API

--- main.js
+++ main.js
@@ -1,1 +1,1 @@
-console.log("test webpack");
+(()=>{"use strict";const t=document.getElementById("form"),e=document.getElementById("city");t.addEventListener("submit",(t=>{t.preventDefault(),async function(t){const e=await fetch(`http://api.weatherapi.com/v1/current.json?key=d679f5647a6d4e16a6522939232510&q=${t}`,{mode:"cors"}),o=await e.json();console.log(o);const n=`${o.location.name}, ${o.location.region}`;console.log(n);const c=o.current.temp_f;console.log(c);const a=o.current.condition.text;console.log(a)}(e.value),e.value=""}))})();
