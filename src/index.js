fa88fc00ebacaff0005902d5efad1be2df4a0a44
yari-dewalt <yari.dewalt@gmail.com>
2023-10-24 20:36:44 -0700

    Setup simple format to test weather API

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
+import { test_function } from "./weatherapi";
 
+const form = document.getElementById("form");
+const input = document.getElementById("city");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  test_function(input.value);
+  input.value = "";
+});
