39a1dd88eeef96650dad8840d747c4cb11e5bf44
yari-dewalt <yari.dewalt@gmail.com>
2023-10-27 16:51:37 -0700

    Add functionality to search and get results from location

--- src/index.js
+++ src/index.js
@@ -1,10 +1,40 @@
-import { test_function } from "./weatherapi";
+import { get_current_data } from "./weatherapi";
+import { three_day_forecast } from "./weatherapi";
 
 const form = document.getElementById("form");
 const input = document.getElementById("city");
+let current_location = document.getElementById("current-location");
+let current_region = document.getElementById("current-region");
+let current_date_time = document.getElementById("current-date-time");
+let current_temperature = document.getElementById("current-temperature");
+let current_weather = document.getElementById("current-weather");
+let current_weather_icon = document.getElementById("current-weather-icon");
 
-form.addEventListener("submit", (event) => {
+form.addEventListener("submit", async (event) => {
   event.preventDefault();
-  test_function(input.value);
+
+  const info = await get_current_data(input.value);
+
+  const location = info[0];
+  current_location.textContent = location;
+
+  const region = info[1];
+  current_region.textContent = region;
+
+  const date_time = info[2];
+  current_date_time.textContent = date_time;
+
+  const temperature_fahrenheit = info[3];
+  current_temperature.textContent = temperature_fahrenheit;
+
+  const condition = info[4];
+  current_weather.textContent = condition;
+
+  const condition_icon = info[5];
+  current_weather_icon.setAttribute("src", `https://${condition_icon}`);
+
+  console.log(info);
   input.value = "";
 });
+
+three_day_forecast("Redding", 3);
--- src/weatherapi.js
+++ src/weatherapi.js
@@ -1,6 +1,8 @@
+import { format } from "date-fns";
+
 const API_KEY = "d679f5647a6d4e16a6522939232510";
 
-async function test_function(city_name) {
+async function get_current_data(city_name) {
   const response = await fetch(
     `http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${city_name}`,
     { mode: "cors" }
@@ -9,14 +11,68 @@ async function test_function(city_name) {
   const weather_data = await response.json();
   console.log(weather_data);
 
-  const location = `${weather_data.location.name}, ${weather_data.location.region}`;
+  const location = weather_data.location.name;
   console.log(location);
+
+  const region = weather_data.location.region;
+  console.log(region);
+
+  const localtime = weather_data.location.localtime;
+  const date = localtime.split(" ")[0];
+  const time = localtime.split(" ")[1];
+  let year = date.split("-")[0];
+  let month = date.split("-")[1];
+  let day = date.split("-")[2];
+  let hours = time.split(":")[0];
+  let minutes = time.split(":")[1];
+  const date_time = format(
+    new Date(year, String(parseInt(month) - 1), day, hours, minutes),
+    "iiii, do LLL ''yy hh:mm b"
+  );
+
+  console.log(date_time);
 
   const temperature_fahrenheit = weather_data.current.temp_f;
   console.log(temperature_fahrenheit);
 
   const condition = weather_data.current.condition.text;
   console.log(condition);
+
+  const condition_icon = weather_data.current.condition.icon.substring(2);
+
+  return [
+    location,
+    region,
+    date_time,
+    temperature_fahrenheit,
+    condition,
+    condition_icon,
+  ];
 }
 
-export { test_function };
+async function three_day_forecast(city_name, days) {
+  const response = await fetch(
+    `http://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${city_name}&days=${days}`,
+    { mode: "cors" }
+  );
+
+  const weather_data = await response.json();
+
+  const current_data = weather_data.current;
+  console.log(current_data);
+
+  const forecast_data = weather_data.forecast;
+  console.log(forecast_data);
+
+  const first_day_data = forecast_data.forecastday[0];
+  console.log(first_day_data);
+
+  const second_day_data = forecast_data.forecastday[1];
+  console.log(second_day_data);
+
+  const third_day_data = forecast_data.forecastday[2];
+  console.log(third_day_data);
+}
+
+export { get_current_data };
+export { three_day_forecast };
